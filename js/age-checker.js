fc3aa64cc4044da362bf81e83d3a5f2125013197
luispegasgomes <luispegasgomes@gmail.com>
2021-03-04 23:01:11 +0000

    errors correction

--- js/age-checker.js
+++ js/age-checker.js
@@ -1,1 +1,14 @@
-alert('Hello')
+//btn verify
+function verify() {
+    let date = new Date()
+    let year = date.getFullYear()
+    let fyear = document.getElementById('txtyear')
+    let res = document.querySelector('div#res')
+    if (fyear.value.length == 0 || Number(fyear.value) > year) {
+        window.alert('This values are wrong, try again :(')
+    }else{
+        let fsex = document.getElementsByName('radsex')
+        let age = year - Number(fyear.value)
+        res.innerHTML = `Calculated age: ${age}`
+    }
+}
